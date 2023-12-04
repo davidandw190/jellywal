8b16d42944635623ac45f01738e82554414c7409
Andrei-David Nan <nandavid190@gmail.com>
2023-12-04 10:23:38 +0200

    v0.2 - add needed structures, add validation func;

--- jellywal.go
+++ jellywal.go
@@ -1,6 +1,9 @@
 package jellywal
 
-import "os"
+import (
+	"os"
+	"sync"
+)
 
 const (
 	DefaultSegmentSize = 20 * 1024 * 1024 // 20 MB
@@ -8,7 +11,7 @@ const (
 	DefaultFilePerms   = 0640
 )
 
-// Options for configuring the log
+// Config for configuring the log
 type Config struct {
 	Sync        bool        // Enable fsync after writes for more durability
 	SegmentSize int         // Size of each log segment. Default is 20 MB.
@@ -16,10 +19,60 @@ type Config struct {
 	FilePerms   os.FileMode // Log file permissions.
 }
 
-// DefaultOptions for the log
-var Defaultconfig = &Config{
+// DefaultConfig for the log
+var DefaultConfig = &Config{
 	Sync:        true, // Fsync after every write
 	SegmentSize: DefaultSegmentSize,
 	DirPerms:    DefaultDirPerms,
 	FilePerms:   DefaultFilePerms,
+}
+
+// Log represents a write-ahead log, also known as an append only log
+type Log struct {
+	mu       sync.RWMutex
+	path     string     // Absolute path to log directory
+	segments []*segment // All known log segments
+	sfile    *os.File   // Tail segment file handle
+	wbatch   Batch      // Reusable write batch
+
+	config  Config
+	closed  bool
+	corrupt bool
+}
+
+// Segment represents a single segment file.
+type segment struct {
+	path  string    // Path of the segment file
+	index uint64    // First index of the segment
+	cbuf  []byte    // Cached entries buffer
+	cpos  []bytepos // Cached entries positions in the buffer
+}
+
+// bpos represents byte positions in a buffer
+type bytepos struct {
+	start int // Byte position
+	end   int // One byte past pos
+}
+
+type Batch struct {
+	entries []batchEntry
+	datas   []byte
+}
+
+type batchEntry struct {
+	size int
+}
+
+func (c *Config) Validate() {
+	if c.SegmentSize <= 0 {
+		c.SegmentSize = DefaultSegmentSize
+	}
+
+	if c.DirPerms == 0 {
+		c.DirPerms = DefaultDirPerms
+	}
+
+	if c.FilePerms == 0 {
+		c.FilePerms = DefaultFilePerms
+	}
 }
